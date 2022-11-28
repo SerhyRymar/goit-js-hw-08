3b7cdde9558a9bca630bdba865e41db00e665cfd
SerhyRymar <serhyrymar@gmail.com>
2022-11-28 19:31:21 +0200

    Add_task02

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// 1.Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї
+const galleryContainer = document.querySelector(".gallery");
+
+
+const craetePictureCardsMarkup = galleryItems.map(item =>
+      `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" 
+    alt="${item.description}"/>
+    </a>`
+  )
+  .join('');
+
+galleryContainer.insertAdjacentHTML('beforeend', craetePictureCardsMarkup);
+let modal = new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
